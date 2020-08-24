158ba9056de8193737ceca247f9387e129005fad
Sam <samuel_morgan_@outlook.com>
2020-08-24 18:56:08 +0100

    Moved btn to under grid

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Grid from './Grid';
 function App() {
 	const [ cells, setCells ] = useState([]);
 	return (
-		<div className="App">
+		<div className="app">
 			<Grid dim={8} cells={cells} setCells={setCells} />
 			<button onClick={() => setCells([])}>Reset</button>
 		</div>
