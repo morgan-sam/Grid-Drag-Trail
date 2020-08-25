c668e7c115d7a55c9958b4b7dec19d638b8ebce9
Sam <samuel_morgan_@outlook.com>
2020-08-25 18:31:15 +0100

    Drag uses new array fill null

--- src/comps/App.jsx
+++ src/comps/App.jsx
@@ -4,8 +4,9 @@ import Grid from 'comps/Grid.jsx';
 
 function App() {
 	const [ dim, setDim ] = useState(6);
-	const [ drag, setDrag ] = useState([ null, null, null ]);
+	const [ drag, setDrag ] = useState(new Array(3).fill(null));
 	const [ cells, setCells ] = useState([]);
+	
 	return (
 		<div className='app'>
 			<div className="grid-and-btn">
