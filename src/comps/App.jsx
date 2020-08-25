4132d5e457eb22331286e7f5b2cd41723d03e9cd
Sam <samuel_morgan_@outlook.com>
2020-08-25 21:29:24 +0100

    symbol state skeleton

--- src/comps/App.jsx
+++ src/comps/App.jsx
@@ -3,6 +3,7 @@ import 'css/App.css';
 import Grid from 'comps/Grid.jsx';
 
 function App() {
+	const [ symbol, setSymbol ] = useState('arrow');
 	const [ dim, setDim ] = useState(6);
 	const [ drag, setDrag ] = useState(new Array(3).fill(null));
 	const [ cells, setCells ] = useState([]);
@@ -10,10 +11,13 @@ function App() {
 	return (
 		<div className='app'>
 			<div className="grid-and-btn">
-				<Grid {...{dim, drag, setDrag, cells, setCells}} />
+				<Grid {...{symbol, dim, drag, setDrag, cells, setCells}} />
 				<div className='btn-container'>
 					<button className='btn' onClick={() => setCells([])}>Reset</button>
-					<button className='btn' onClick={() => null}>Null</button>
+					<button className='btn' onClick={() => setSymbol((cur)=>{
+						if (cur === 'arrow') return 'rail'
+						else return 'arrow'
+					})}>Symbol</button>
 				</div>
 			</div>
 		</div>
